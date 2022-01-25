6a184cfb19af2c1e2b4cdf7634abc946723dc7b3
Gabriel Reina <dreinaj@gmail.com>
2022-01-25 14:41:41 -0700

    quiz layout

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var questionBank = [
+    {
+        question: "What is HTML ?",
+        choices: [ "Hypertext Makeup language","Hypertext Markup language","Hitkertext markup language","Huajsld"],
+        answer: 1
+    },
+    {
+        question: "2 What is HTML ?",
+        choices: [ "Hypertext Makeup language","Hypertext Markup language","Hitkertext markup language","Huajsld"],
+        answer: 1
+    },
+    {
+        question: "3What is HTML ?",
+        choices: [ "Hypertext Makeup language","Hypertext Markup language","Hitkertext markup language","Huajsld"],
+        answer: 1
+    },
+    {
+        question: "4 What is HTML ?",
+        choices: [ "Hypertext Makeup language","Hypertext Markup language","Hitkertext markup language","Huajsld"],
+        answer: 1
+    }
+]
