c20dba12af60301c4f087db6c46832463648848c
Gabriel Reina <dreinaj@gmail.com>
2022-01-25 15:09:06 -0700

    timer and first question

--- javascript.js
+++ javascript.js
@@ -20,5 +20,30 @@ var questionBank = [
         answer: 1
     }
 ]
+var question=0
+var score=0
+var timer=document.getElementById("timer")
+var counter=30
+var timerelement;
+
 var questionContainer=document.getElementById("questioncontainer")
 questionContainer.style.display="none"
+var startButton=document.getElementById("startbutton")
+startButton.addEventListener("click",function(){
+    questionContainer.style.display="block"
+    startButton.style.display="none"
+    timerelement=setInterval(function(){
+        timer.textContent="timeleft"+counter
+        if (counter>1){
+            counter--;
+        }
+    },1000) 
+    questiondisplay()
+})
+function questiondisplay(){
+    document.getElementById("displayquestion").textContent=questionBank[question].question
+    document.getElementById("answer1").textContent=questionBank[question].choices[0]
+    document.getElementById("answer2").textContent=questionBank[question].choices[1]
+    document.getElementById("answer3").textContent=questionBank[question].choices[2]
+    document.getElementById("answer4").textContent=questionBank[question].choices[3]
+}
